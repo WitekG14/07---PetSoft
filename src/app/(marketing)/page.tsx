6bc7375a2605be186ca5800c784c3852671a35ae
WitekG14 <witold@grzegorczyk.it>
2026-03-13 11:49:08 +0000

    Shadcn UI Intro & Setup

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,16 +15,21 @@ export default function Home() {
 
       <div>
         <Logo />
-        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+        <h1 className="text-5xl font-semibold my-6 max-w-125">
           Manage your <span className="font-extrabold">pet daycare</span> with
           ease
         </h1>
-        <p className="text-2xl font-medium max-w-[600px]">
+        <p className="text-2xl font-medium max-w-150">
           Use PetSoft to easily keep track of pets under your care. Get lifetime
           access for $299.
         </p>
-        <div className="mt-10">
-          <button>Sign up</button>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log in</Link>
+          </Button>
         </div>
       </div>
     </main>
