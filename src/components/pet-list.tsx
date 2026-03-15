8d7dd59f39a06a8ed3d8d6dcc78108dd0fb4e3dc
WitekG14 <witold@grzegorczyk.it>
2026-03-15 17:53:54 +0000

    PetList: Add Pet + Styling

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 function PetList() {
   return (
     <ul className="bg-white border-b border-black/8">
       <li>
-        <button>Benjamin</button>
+        <button className="flex items-center h-17.5 w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
       </li>
     </ul>
   );
